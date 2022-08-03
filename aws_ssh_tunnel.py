3acfbe7a3172811c0cfaded27a266a72e845454f
Daniel Molenaars <danielmolenaars@binx.io>
2022-08-03 15:00:04 +0200

    build: add direct shell support

--- aws_ssh_tunnel.py
+++ aws_ssh_tunnel.py
@@ -11,6 +11,7 @@ including instances that can only be accessed from within a designated VPC or se
 
 import os
 import random
+import socket
 import subprocess
 import sys
 import boto3
@@ -21,6 +22,20 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 DEFAULT_CFG_FILE = os.path.join(__location__, "aws_ssh_tunnel.cfg")
 RUNNING_STATE_CODE = 16
 cfg = ConfigParser()
+
+
+def common_options(function):
+    function = click.option(
+        "--tag",
+        "-t",
+        type=str,
+        help="tag (format: KEY=VALUE) of the (jump) instance that will be used to set up the SSH (tunneling) session."
+        " If tunneling to RDS or other services which only allow internal vpc traffic,"
+        " pass the tag of a dedicated jump instance. Omit to use the tag environment variable"
+        " in the local configuration file.",
+        show_default="ssh_instance_tag environment variable in aws-ssh-tunnel.cfg",
+    )(function)
+    return function
 
 
 @click.group()
@@ -42,7 +57,7 @@ def get_aws_session(ctx):
 
 
 @click.pass_context
-def load_config(ctx, remote_host, port, ssh_instance_tag):
+def load_config(ctx, tag, remote_host, port):
     """
     Load the AWS environment variables from the configuration file as well as any additional parameters.
     """
@@ -51,8 +66,8 @@ def load_config(ctx, remote_host, port, ssh_instance_tag):
         ctx.obj = dict(cfg["aws_environment"])
         ctx.obj["port"] = port
         ctx.obj["remote_host"] = remote_host
-        if ssh_instance_tag is not None:
-            ctx.obj["ssh_instance_tag"] = ssh_instance_tag
+        if tag is not None:
+            ctx.obj["ssh_instance_tag"] = tag
     else:
         click.echo(
             "Unable to retrieve AWS environment variables,"
@@ -81,36 +96,28 @@ def set_target_instance_details(ctx, session):
         ssh_instance_id = random_instance["InstanceId"]
         ssh_instance_az = random_instance["Placement"]["AvailabilityZone"]
         click.echo(
-            f"Found instance with tag {ctx.obj['ssh_instance_tag']}"
+            f"Found instance with tag {ctx.obj['tag']}"
             f" and id {ssh_instance_id} on availability zone {ssh_instance_az}..."
         )
         ctx.obj["ssh_instance_id"] = ssh_instance_id
         ctx.obj["ssh_instance_az"] = ssh_instance_az
     else:
         click.echo(
-            f"No instances found with tag {ctx.obj['ssh_instance_tag']} and profile {ctx.obj['aws_profile']}"
+            f"No instances found with tag {ctx.obj['tag']} and profile {ctx.obj['aws_profile']}"
             f" in region {ctx.obj['aws_region']}, exiting..."
         )
         sys.exit(1)
 
 
-@click.pass_context
-def start_tunnel(ctx):
-    """
-    Start a tunneling session.
-    Can be a direct tunnel to the target EC2 instance or a tunnel to a second instance using a jump server.
-    """
-    click.echo(
-        f"Attempting to start tunnel on AWS SSM Session Manager to {ctx.obj['remote_host']}"
-        f" on port {ctx.obj['port']}..."
-    )
-    params = f'host="{ctx.obj["remote_host"]}",portNumber="{ctx.obj["port"]}",localPortNumber="{ctx.obj["port"]}"'
-    cmd = f"aws ssm start-session \
-         --target {ctx.obj['ssh_instance_id']} \
-         --document-name AWS-StartPortForwardingSessionToRemoteHost \
-         --parameters {params} \
-         --profile {ctx.obj['aws_profile']} \
-         --region {ctx.obj['aws_region']}"
+def get_free_port():
+    s = socket.socket()
+    s.bind(("", 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
+
+def execute_ssm_command(cmd):
     ssh_proc = subprocess.Popen(
         cmd,
         shell=True,
@@ -120,11 +127,51 @@ def start_tunnel(ctx):
         stdout, stderr = ssh_proc.communicate()
         if stderr:
             print(str(stderr))
-
     except KeyboardInterrupt:
         ssh_proc.terminate()
         click.echo("\nClosing application...")
         sys.exit(0)
+
+
+@click.pass_context
+def start_tunnel(ctx, local_port):
+    """
+    Start a tunneling session.
+    Can be a direct tunnel to the target EC2 instance or a tunnel to a second instance using a jump server.
+    """
+    click.echo(
+        f"Attempting to start tunnel on AWS SSM Session Manager to {ctx.obj['remote_host']}"
+        f" using local port {local_port} and remote port {ctx.obj['port']}..."
+    )
+    params = f'host="{ctx.obj["remote_host"]}",portNumber="{ctx.obj["port"]}",localPortNumber="{local_port}"'
+    cmd = f"aws ssm start-session \
+         --target {ctx.obj['ssh_instance_id']} \
+         --document-name AWS-StartPortForwardingSessionToRemoteHost \
+         --parameters {params} \
+         --profile {ctx.obj['aws_profile']} \
+         --region {ctx.obj['aws_region']}"
+    execute_ssm_command(cmd)
+
+
+@click.pass_context
+def start_session(ctx):
+    """
+    Start an ssh session.
+    """
+    click.echo(
+        f"Attempting to start session on AWS SSM Session Manager to {ctx.obj['ssh_instance_id']}..."
+    )
+    cmd = f"aws ssm start-session \
+         --target {ctx.obj['ssh_instance_id']} \
+         --profile {ctx.obj['aws_profile']} \
+         --region {ctx.obj['aws_region']}"
+    execute_ssm_command(cmd)
+
+
+def initialize_environment(tag, remote_host=None, port=None):
+    load_config(tag, remote_host, port)
+    session = get_aws_session()
+    set_target_instance_details(session)
 
 
 @main.command()
@@ -146,7 +193,7 @@ def config():
         "AWS profile to assume for tunneling session",
         default=aws_config.get("aws_profile"),
     )
-    ssh_instance_tag = click.prompt(
+    tag = click.prompt(
         "Tag used to identify the (jump) instance that will be used to set up the SSH session",
         default=aws_config.get("ssh_instance_tag"),
     )
@@ -155,7 +202,7 @@ def config():
         **{
             "aws_region": aws_region,
             "aws_profile": aws_profile,
-            "ssh_instance_tag": ssh_instance_tag,
+            "ssh_instance_tag": tag,
         },
     }
 
@@ -166,12 +213,12 @@ def config():
 
 
 @main.command()
+@common_options
 @click.option(
     "-r",
-    "--remote_host",
+    "--remote-host",
     type=str,
-    help="Remote host endpoint to to jump to."
-    " Omit or set to 'localhost' to set up a direct tunnel with the instance defined in '--tag'",
+    help="Remote host endpoint to tunnel to.",
     default="localhost",
     show_default=True,
 )
@@ -179,38 +226,52 @@ def config():
     "--port",
     "-p",
     type=str,
-    help="Listening port on the remote host. The same port will be opened on the local machine.",
-    default=33333,
+    help="The port on the remote host to forward traffic to.",
+    default=22,
     show_default=True,
 )
-@click.option(
-    "--ssh_instance_tag",
-    "-t",
-    type=str,
-    help="tag (format: KEY=VALUE) of the (jump) instance that will be used to set up the SSH session."
-    " If tunneling to RDS or other services which only allow internal vpc traffic,"
-    " pass the tag of a dedicated jump instance. Omit to use the ssh_instance_tag environment variable"
-    " in the local configuration file.",
-    show_default="ssh_instance_tag environment variable in aws-ssh-tunnel.cfg",
-)
-def run(remote_host, port, ssh_instance_tag):
+def start_forwarding_session(tag, remote_host, port):
     """
-    Start the CLI.
+    Start a port forwarding session to the specified remote host.
 
     Example:
 
-    aws-ssh-tunnel run \n
-        --remote_host mydb.123456789012.eu-west-1.rds.amazonaws.com \n
+    aws-ssh-tunnel start-forwarding-session \n
+        --remote-host mydb.123456789012.eu-west-1.rds.amazonaws.com \n
         --port 5432 \n
-        --ssh_instance_tag application=jump_server \n
+        --tag application=jump_server \n
     """
     try:
-        load_config(remote_host, port, ssh_instance_tag)
-        session = get_aws_session()
-        set_target_instance_details(session)
-        start_tunnel()
+        initialize_environment(
+            tag,
+            remote_host,
+            port,
+        )
+        local_port = get_free_port()
+        start_tunnel(local_port)
     except Exception as error:
-        click.echo(f"Something went wrong while executing the CLI: {error}")
+        click.echo(
+            f"Something went wrong when starting the port forwarding session: {error}"
+        )
+
+
+@main.command()
+@common_options
+def start_ssh_session(tag):
+    """
+    Start an SSH session on the specified host.
+
+    Example:
+
+    aws-ssh-tunnel start-ssh-session \n
+        -t application=jump_server \n
+    """
+    try:
+        initialize_environment(tag)
+        start_session()
+
+    except Exception as error:
+        click.echo(f"Something went wrong when starting the SSH session: {error}")
 
 
 if __name__ == "__main__":
